43818f1dbb8a1f17fa3f1fc158ac2000e810e924
Obelisk <camcooke10@gmail.com>
2021-04-24 18:04:47 -0700

    Initial commit, wonky website, working on mediarecorder API

---
+++ jScript.js
@@ -0,0 +1,45 @@
+var slideIndex = 1;
+      showSlides(slideIndex);
+    const stopButton = document.getElementById()
+    const record = function(stream) {
+      const options = {mimeType: 'audio/webm'}; 
+      const record = []
+      const Recorder = new MediaRecorder(stream, options);
+      Recorder.addEventListener('dataavailable', function (yuh)
+      {
+        if (yuh.data.size > 0)
+        {
+            record.push(yuh.data);
+        }
+        if(stopTime == true && stopped == false)
+        {
+            Recorder.stop();
+            stopped = true;
+        } 
+      });
+    }
+      // Next/previous controls
+      function plusSlides(n) {
+        showSlides(slideIndex += n);
+      }
+
+      // Thumbnail image controls
+      function currentSlide(n) {
+        showSlides(slideIndex = n);
+      }
+
+      function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+      }
