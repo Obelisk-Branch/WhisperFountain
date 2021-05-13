d617ec4d9ff40300893ec8ec0c1617d10bdb1984
Obelisk <camcooke10@gmail.com>
2021-05-12 22:03:01 -0700

    You can record and download your voice file with the buttons now!

--- jScript.js
+++ jScript.js
@@ -1,6 +1,7 @@
     var startB, stopB;
-    startB = document.getElementById(startButton);
-    stopB = document.getElementById(stopButton);
+    startB = document.getElementById("startButton");
+    stopB = document.getElementById("stopButton");
+    downB = document.getElementById("downloadButton");
     if (navigator.mediaDevices.getUserMedia)
     {
       console.log("getUserMedia isn't broken! Woo!");
@@ -11,56 +12,91 @@
       navigator.mediaDevices.getUserMedia({
         audio: true
       })
-      .then(function(stream) {
-        const options = {mimeType: 'audio/webm'}; 
-        const record = [];
-        const Recorder = new MediaRecorder(stream, options);
+    
+      let Success = function(stream) {
+        var options = {mimeType: 'audio/webm'}; 
+        var record = [];
+        var stopped = false;
+        var stopTime = false;
+        var Recorder = new MediaRecorder(stream, options);
         console.log("initialized mediaRecorder");
-        startB.addEventListener('click', startRecording);
-        stopB.addEventListener('click', stopRecording);
-        Recorder.addEventListener('dataavailable', yuh);
+        //startB.addEventListener('click', startRecording);
+        //stopB.addEventListener('click', stopRecording);
+        //Recorder.addEventListener('dataavailable', yuh);
 
 
-      });
-        function startRecording()
+      
+        startB.onclick = function startRecording()
         {
-          Recorder.start()
-
+          Recorder.start();
+          startB.style.background = "red";
+          stopB.style.background = "red";
         }
         
 
-        function stopRecording() {
+        stopB.onclick = function stopRecording() {
 
-          Recorder.stop()
-
+          Recorder.stop();
+          startB.style.background = "blue";
+          stopB.style.background = "blue";
+          //downloadTime(record);
+          stopTime = true;
         }
-        function yuh(e)
+        Recorder.ondataavailable = function(yuh)
         {
             if (yuh.data.size > 0)
             {
                 record.push(yuh.data);
-                download();
             }
-            if(stopTime == true && stopped == false)
+            if(stopTime == true)
             {
                 Recorder.stop();
                 stopped = true;
+                //downloadTime(record);
+
             } 
         }
+        downB.onclick = function downloadTime()
+        {
+          var blob = new Blob(record, {
+            type: "audio/webm"
+          });
+          record = []
+          var a = document.createElement("a");
+          var url = URL.createObjectURL(blob);
+          document.body.appendChild(a);
+          a.style = "display: none";
+          a.href = url;
+          a.download = "test.webm";
+          a.click();
+          window.URL.revokeObjectURL(url);
+        }
+
+        function downloadTime2(record) {
+          var blob = new Blob(record, {
+            type: "audio/webm"
+          });
+          record = []
+          var a = document.createElement("a");
+          var url = URL.createObjectURL(blob);
+          document.body.appendChild(a);
+          a.style = "display: none";
+          a.href = url;
+          a.download = "test.webm";
+          a.click();
+          window.URL.revokeObjectURL(url);
+        }
+      
+
+
+
      
         }
-      function download() {
-        var blob = new Blob(record, {
-          type: "audio/webm"
-        });
-
-        var a = document.createElement("a");
-        document.body.appendChild(a);
-        a.style = "display: none";
-        a.href = url;
-        a.download = "test.webm";
-        a.click();
-        window.URL.revokeObjectURL(url);
+      var error = function(err){
+        console.log("GetUserMedia didn't work, RIP");
       }
-    
+        navigator.mediaDevices.getUserMedia(constraints).then(Success, error);
+
+    }
+     
    
